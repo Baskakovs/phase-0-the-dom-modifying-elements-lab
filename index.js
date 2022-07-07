d6927f22695386fc4f605184b1b69e04c7a3168c
Baskakovs <baskakovs@me.com>
2022-07-07 09:31:53 +0300

    commit tree 2312df3252272910f181d9da98657ee0b3d9e6db

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+const main = document.querySelector("main")
+const body = document.querySelector('body')
+main.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = 'Peter is the champion'
+body.append(newHeader)
